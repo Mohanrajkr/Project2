58f750dec2c41046bb9013d264bfe731aec57ae3
Mohanrajkr <mohanraj260795@gmail.com>
2017-07-13 16:51:33 +0530

    Add files via upload

--- Restfulservices/src/main/java/com/niit/collaborationback/controller/UserController.java
+++ Restfulservices/src/main/java/com/niit/collaborationback/controller/UserController.java
@@ -4,6 +4,9 @@ package com.niit.collaborationback.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.niit.collaborationback.dao.UserDAO;
@@ -48,7 +53,7 @@ public class UserController {
 		return new ResponseEntity(user, HttpStatus.OK);
 	}
 	
-	@SuppressWarnings("unchecked")
+	
 	@PostMapping(value = "/user")
 	public ResponseEntity createUser(@RequestBody User user) {
 
@@ -80,5 +85,38 @@ public class UserController {
 		return new ResponseEntity(user, HttpStatus.OK);
 	}
 
-	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public ResponseEntity<?> login(@RequestBody User user,HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		User validUser = userDAO.login(user);
+		if (validUser == null) {
+			Error error = new Error("Invalid credentials.. please enter valid username and password");
+			return new ResponseEntity<Error>(error, HttpStatus.UNAUTHORIZED);
+		} else {
+			session.setAttribute("user", validUser);
+			
+			
+			System.out.println(validUser.getEmail());
+			System.out.println(validUser.getUserName());
+			User user1 = (User) session.getAttribute("user"); 
+			System.out.println(user1.getRole());
+			System.out.println(user1.getMobileNumber());
+			return new ResponseEntity<User>(validUser, HttpStatus.OK);
+		}
+	}
+	@RequestMapping(value="/logout",method=RequestMethod.PUT)
+	public ResponseEntity<?> logout(HttpSession session){
+		User user=(User)session.getAttribute("user");
+		if(user==null){
+			Error error =new Error("Unauthorized user.. Please Login..");  
+			return new ResponseEntity<Error>(error,HttpStatus.UNAUTHORIZED);
+		}
+		else{
+			//user.setOnline(false);
+			userDAO.saveOrUpdate(user);
+			session.removeAttribute("user");
+			session.invalidate();
+			return new ResponseEntity<Void>(HttpStatus.OK);
+		}
+	}
 }
